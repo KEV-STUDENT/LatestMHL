cd30937317615709a93eed7e523fb954e5b179aa
Egor <9529777@gmail.com>
2023-01-23 17:36:08 +0300

    Picker Refactoring
    Picker Refactoring

--- ControlsCommon/Models/MPickerString.cs
+++ ControlsCommon/Models/MPickerString.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MHLCommon;
+using MHLCommon.MHLScanner;
 
 namespace ControlsCommon.Models
 {
-    internal class MPicker
+    public class MPickerString : IMPicker<string>
     {
+        #region [IMPicker]
+        string IMPicker<string>.Value { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        bool IMPicker<string>.IsReadOnlyTextInput { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        event Action<IPicker<string>>? IMPicker<string>.AskUserEntry
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        ReturnResultEnum IMPicker<string>.CheckValue()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
--- ControlsCommon/ViewModels/VMPicker.cs
+++ ControlsCommon/ViewModels/VMPicker.cs
@@ -1,14 +1,25 @@
 ﻿using ControlsCommon.ControlsViews;
+using MHLCommon.ViewModels;
+using System.Windows.Input;
 
 namespace ControlsCommon.ViewModels
 {
-    public class VMPicker :IVMPicker
+    public class VMPicker<T> : ViewModel, IVMPicker<T>
     {
         private IPickerView view;
 
-        public VMPicker(IPickerView customPicker)
+        #region [Constructors]
+        public VMPicker(IPickerView viewUI)
         {
-            this.view = customPicker;
+            this.view = viewUI;
         }
+
+        #region [IVMPicker]
+        ICommand IVMPicker.AskUserEntryCommand { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        void IVMPicker.ExecuteAskUserEntryCommand(object? obj)
+        { _model.AskUserEntryAction(); }
+        bool IVMPicker.CanExecuteAskUserEntryCommand(object? obj)
+        { return _model.AskUserEntryCanExecute(); }
+        #region
     }
 }
