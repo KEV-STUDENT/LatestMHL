d1c1de81ffe1b400d728e26b3e05eabb742a0849
Egor <9529777@gmail.com>
2023-01-26 11:46:33 +0300

    VUE - Lesson1

---
+++ VUESourceScanner/vue_ss/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="increment">Increment</button>
+        <button @click="decrement">Decriment</button>
+    </div>
+    <div> Тест работает !!! Count = {{ count }}</div>
+</template>
+<script>
+import { defineSSRCustomElement } from 'vue';
+
+export default{
+    data(){
+       return { count : 0}
+    },
+    methods:{
+        increment(){
+            this.count += 1
+        },
+        decrement(){
+            this.count -= 1
+        }
+    }
+}
+</script>
+<style></style>
